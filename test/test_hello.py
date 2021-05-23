5ca02b79dd045429b58035bd281370852c2118d8
ControlNet <smczx@hotmail.com>
2021-05-24 01:23:11 +1000

    add unittest to github action

--- test/test_hello.py
+++ test/test_hello.py
@@ -3,6 +3,9 @@ import unittest
 from tensorneko.hello import return_hello
 
 
+import os
+print(os.getenv("PYTHONPATH"))
+
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(return_hello(), "Hello TensorNeko")
