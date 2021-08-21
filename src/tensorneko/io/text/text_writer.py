25805054590b35336a340d9ecd046c1d2b40c8b1
ControlNet <smczx@hotmail.com>
2021-08-21 10:11:03 +1000

    add documentation for writers

--- src/tensorneko/io/text/text_writer.py
+++ src/tensorneko/io/text/text_writer.py
@@ -5,24 +5,52 @@ from pandas import DataFrame
 
 
 class TextWriter:
+    """TextWriter for writing files for text"""
 
     @staticmethod
-    def to_plain(path: str, text: str, encoding="UTF-8"):
+    def to_plain(path: str, text: str, encoding="UTF-8") -> None:
+        """
+        Save as a plain text file.
+
+        Args:
+            path (``str``): The path of output file.
+            text (``str``): The content for output.
+            encoding (``str``, optional): Python file IO encoding parameter. Default: "UTF-8".
+        """
         with open(path, "w", encoding=encoding) as file:
             file.write(text)
 
     @staticmethod
     @overload
-    def to_json(path: str, obj: DataFrame, encoding="UTF-8"):
+    def to_json(path: str, obj: DataFrame, orient: str = None) -> None:
+        """
+        Save as Json file from a :class:`~pandas.DataFrame`.
+
+        Args:
+            path (``str``): The path of output file.
+            obj (:class:`~pandas.DataFrame`): The DataFrame used for json output.
+            orient (``str``, optional): The json format option from :meth:`~pandas.DataFrame.to_json`. Default: None.
+        """
         ...
 
     @staticmethod
     @overload
-    def to_json(path: str, obj: Union[dict, list], orient: str):
+    def to_json(path: str, obj: Union[dict, list], encoding="UTF-8") -> None:
+        """
+        Save as Json file from a dictionary or list.
+
+        Args:
+            path (``str``): The path of output file.
+            obj (``dict`` | ``list``): The json data which need to be used for output.
+            encoding (``str``, optional): Python file IO encoding parameter. Default: "UTF-8".
+        """
         ...
 
     @staticmethod
-    def to_json(path: str, obj: Union[dict, list, DataFrame], encoding="UTF-8", orient=None):
+    def to_json(path: str, obj: Union[dict, list, DataFrame], encoding="UTF-8", orient=None) -> None:
+        """
+        The implementation of :meth:`~TextWriter.to_json` method.
+        """
         if type(obj) in (dict, list):
             with open(path, "w", encoding=encoding) as file:
                 file.write(json.dumps(obj))
@@ -32,7 +60,18 @@ class TextWriter:
             raise TypeError("Not implemented type. Only support dict, list and DataFrame.")
 
     @staticmethod
-    def to_csv(path: str, dataframe: DataFrame, index: bool = True, columns=None, header=None, sep=","):
+    def to_csv(path: str, dataframe: DataFrame, index: bool = True, columns=None, header=True, sep=",") -> None:
+        """
+        Save as csv file from a :class:`~pandas.DataFrame`.
+
+        Args:
+            path (``str``): The path of output file.
+            dataframe (:class:`~pandas.DataFrame`): The data frame used to output csv.
+            index (``bool``, optional): If True then contains index in output file. Default: True
+            columns (``Sequence``, optional): The columns in the data frame for output. Default: All columns.
+            header (``bool``, optional): If True then contains headers in the output file. Default: True.
+            sep (``str``): The string for CSV delimiter.
+        """
         dataframe.to_csv(path, sep=sep, columns=columns, header=header, index=index)
 
     to = to_plain
