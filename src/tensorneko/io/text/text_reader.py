468c67216d484b29afc45ed24f26285cda236b0c
ControlNet <smczx@hotmail.com>
2021-08-24 11:26:49 +1000

    add to_df option for TextReader.of_json

--- src/tensorneko/io/text/text_reader.py
+++ src/tensorneko/io/text/text_reader.py
@@ -1,4 +1,8 @@
+import json
+from typing import Union
+
 import pandas as pd
+from pandas import DataFrame
 
 
 class TextReader:
@@ -20,7 +24,30 @@ class TextReader:
             text = file.read()
         return text
 
-    of_json = pd.read_json
+    @staticmethod
+    def of_json(path: str, to_df: bool = False, orient: str = None, encoding: str = "UTF-8"
+    ) -> Union[dict, list, DataFrame]:
+        """
+        Read json files to :class:`~pandas.DataFrame` or ``list`` or ``dict``.
+
+        Args:
+            path (``str``): Json file path.
+            to_df (``bool``, optional):
+                True then return :class:`~pandas.DataFrame`, else return ``list`` or ``dict``. Default: False
+            orient (``str``, optional): The json format parameter from :func:`pandas.read_json`. Default: None
+            encoding (``str``, optional): The encoding for python ``open`` function. Default: "UTF-8"
+
+        Returns:
+            ``dict`` | ``list`` | :class:`~pandas.DataFrame`: The object of given json.
+        """
+
+        if to_df:
+            return pd.read_json(path, orient=orient)
+        else:
+            with open(path, "r", encoding=encoding) as file:
+                obj = json.loads(file.read())
+            return obj
+
     of_csv = pd.read_csv
     of_xml = pd.read_xml
     of = of_plain
