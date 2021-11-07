b18634300ed43c6f764549937cca741c0ae4f78a
ControlNet <smczx@hotmail.com>
2021-11-07 13:01:46 +1100

    make matplotlib and seaborn optional

--- src/tensorneko/visualization/__init__.py
+++ src/tensorneko/visualization/__init__.py
@@ -1,12 +1,27 @@
-from .matplotlib import imshow
-from .seaborn import barplot
 from .log_graph import log_graph
+from .color import Colors, ContinuousColors
 from . import watcher
 from . import tensorboard
 
+
 __all__ = [
-    "imshow",
     "log_graph",
     "watcher",
-    "tensorboard"
+    "tensorboard",
+    "Colors",
+    "ContinuousColors"
 ]
+
+try:
+    from . import matplotlib
+except ImportError:
+    pass
+else:
+    __all__.append("matplotlib")
+
+try:
+    from . import seaborn
+except ImportError:
+    pass
+else:
+    __all__.append("seaborn")
