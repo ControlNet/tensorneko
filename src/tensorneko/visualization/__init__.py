9b73de022911df0f7ae9a6aa81542a87123c12e2
ControlNet <smczx@hotmail.com>
2021-11-08 14:29:27 +1100

    fix visualization __init__

--- src/tensorneko/visualization/__init__.py
+++ src/tensorneko/visualization/__init__.py
@@ -2,15 +2,6 @@ from .log_graph import log_graph
 from .color import Colors, ContinuousColors
 from . import watcher
 from . import tensorboard
-try:
-    from . import matplotlib
-except ImportError:
-    pass
-
-try:
-    from . import seaborn
-except ImportError:
-    pass
 
 
 __all__ = [
