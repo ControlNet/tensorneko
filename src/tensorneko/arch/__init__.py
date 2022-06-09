e1f8f1700dd979a95b3617f427929032835951df
ControlNet <smczx@hotmail.com>
2022-06-09 19:31:11 +1000

    impl auto encoder architecture

---
+++ src/tensorneko/arch/__init__.py
@@ -0,0 +1,11 @@
+from .gan import GAN
+from .vqvae import VQVAE
+from .wgan import WGAN
+from .auto_encoder import AutoEncoder
+
+__all__ = [
+    "GAN",
+    "VQVAE",
+    "WGAN",
+    "AutoEncoder"
+]
