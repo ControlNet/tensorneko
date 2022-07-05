983b05e5294e78d9477d7ae62c1c5dfff2a6e7ae
ControlNet <smczx@hotmail.com>
2022-07-06 04:33:40 +1000

    move tensorboard server to tensorneko_util

--- src/tensorneko_util/visualization/__init__.py
+++ src/tensorneko_util/visualization/__init__.py
@@ -2,8 +2,9 @@ from .color import Colors, ContinuousColors
 from . import matplotlib
 from .multi_plots import MultiPlots
 from . import watcher
+from .tensorboard import Server
 
-__all__ = ["Colors", "ContinuousColors", "MultiPlots", "matplotlib", "watcher"]
+__all__ = ["Colors", "ContinuousColors", "MultiPlots", "matplotlib", "watcher", "Server"]
 
 try:
     from . import seaborn
