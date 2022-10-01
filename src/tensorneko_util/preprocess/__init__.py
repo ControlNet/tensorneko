e77ace753443c7a09cfa81e11cbc35a04e831725
ControlNet <smczx@hotmail.com>
2022-10-01 21:39:32 +1000

    impl rgb2gray

--- src/tensorneko_util/preprocess/__init__.py
+++ src/tensorneko_util/preprocess/__init__.py
@@ -1,7 +1,8 @@
 from ._ffmpeg_check import ffmpeg_available
 from .video import frames2video
+from .image import rgb2gray, rgb2gray_batch
 
-__all__ = ['ffmpeg_available', 'frames2video']
+__all__ = ['ffmpeg_available', 'frames2video', 'rgb2gray', 'rgb2gray_batch']
 
 if ffmpeg_available:
     from .ffmpeg import ffmpeg_command, video2frames, merge_video_audio, resample_video_fps, mp32wav
