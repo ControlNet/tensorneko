6d9529ceb5657a6546696d97f5a9f7b8c5e5845f
ControlNet <smczx@hotmail.com>
2022-10-12 09:27:52 +1100

    fix import plt

--- src/tensorneko_util/backend/visual_lib.py
+++ src/tensorneko_util/backend/visual_lib.py
@@ -10,66 +10,68 @@ class VisualLib(Enum):
     FFMPEG = 5
     SKIMAGE = 6
 
+    @staticmethod
+    def opencv_available() -> bool:
+        if _VisualLibAvailability._is_opencv_available is None:
+            try:
+                import cv2
+                _VisualLibAvailability._is_opencv_available = True
+            except ImportError:
+                _VisualLibAvailability._is_opencv_available = False
+        return _VisualLibAvailability._is_opencv_available
+
+    @staticmethod
+    def pytorch_available() -> bool:
+        if _VisualLibAvailability._is_torchvision_available is None:
+            try:
+                import torchvision
+                _VisualLibAvailability._is_torchvision_available = True
+            except ImportError:
+                _VisualLibAvailability._is_torchvision_available = False
+        return _VisualLibAvailability._is_torchvision_available
+
+    @staticmethod
+    def matplotlib_available() -> bool:
+        if _VisualLibAvailability._is_matplotlib_available is None:
+            try:
+                import matplotlib
+                _VisualLibAvailability._is_matplotlib_available = True
+            except ImportError:
+                _VisualLibAvailability._is_matplotlib_available = False
+        return _VisualLibAvailability._is_matplotlib_available
+
+    @staticmethod
+    def pil_available() -> bool:
+        if _VisualLibAvailability._is_pil_available is None:
+            try:
+                import PIL
+                _VisualLibAvailability._is_pil_available = True
+            except ImportError:
+                _VisualLibAvailability._is_pil_available = False
+        return _VisualLibAvailability._is_pil_available
+
+    @staticmethod
+    def ffmpeg_available() -> bool:
+        if _VisualLibAvailability._is_ffmpeg_available is None:
+            from ..preprocess._ffmpeg_check import ffmpeg_available
+            _VisualLibAvailability._is_ffmpeg_available = ffmpeg_available
+        return _VisualLibAvailability._is_ffmpeg_available
+
+    @staticmethod
+    def skimage_available() -> bool:
+        if _VisualLibAvailability._is_skimage_available is None:
+            try:
+                import skimage
+                _VisualLibAvailability._is_skimage_available = True
+            except ImportError:
+                _VisualLibAvailability._is_skimage_available = False
+        return _VisualLibAvailability._is_skimage_available
+
+
+class _VisualLibAvailability:
     _is_opencv_available: Optional[bool] = None
     _is_torchvision_available: Optional[bool] = None
     _is_matplotlib_available: Optional[bool] = None
     _is_pil_available: Optional[bool] = None
     _is_ffmpeg_available: bool = None
     _is_skimage_available: Optional[bool] = None
-
-    @classmethod
-    def opencv_available(cls) -> bool:
-        if cls._is_opencv_available is None:
-            try:
-                import cv2
-                cls._is_opencv_available = True
-            except ImportError:
-                cls._is_opencv_available = False
-        return cls._is_opencv_available
-
-    @classmethod
-    def pytorch_available(cls) -> bool:
-        if cls._is_torchvision_available is None:
-            try:
-                import torchvision
-                cls._is_torchvision_available = True
-            except ImportError:
-                cls._is_torchvision_available = False
-        return cls._is_torchvision_available
-
-    @classmethod
-    def matplotlib_available(cls) -> bool:
-        if cls._is_matplotlib_available is None:
-            try:
-                import matplotlib
-                cls._is_matplotlib_available = True
-            except ImportError:
-                cls._is_matplotlib_available = False
-        return cls._is_matplotlib_available
-
-    @classmethod
-    def pil_available(cls) -> bool:
-        if cls._is_pil_available is None:
-            try:
-                import PIL
-                cls._is_pil_available = True
-            except ImportError:
-                cls._is_pil_available = False
-        return cls._is_pil_available
-
-    @classmethod
-    def ffmpeg_available(cls) -> bool:
-        if cls._is_ffmpeg_available is None:
-            from ..preprocess._ffmpeg_check import ffmpeg_available
-            cls._is_ffmpeg_available = ffmpeg_available
-        return cls._is_ffmpeg_available
-
-    @classmethod
-    def skimage_available(cls) -> bool:
-        if cls._is_skimage_available is None:
-            try:
-                import skimage
-                cls._is_skimage_available = True
-            except ImportError:
-                cls._is_skimage_available = False
-        return cls._is_skimage_available
