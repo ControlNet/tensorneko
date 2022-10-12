3355b887f9dbd21ce3879a33fbf6d8a656b41187
ControlNet <smczx@hotmail.com>
2022-10-13 00:37:35 +1100

    move crop_with_padding to tensorneko_util

--- src/tensorneko_util/backend/visual_lib.py
+++ src/tensorneko_util/backend/visual_lib.py
@@ -12,66 +12,66 @@ class VisualLib(Enum):
 
     @staticmethod
     def opencv_available() -> bool:
-        if _VisualLibAvailability._is_opencv_available is None:
+        if _VisualLibAvailability.is_opencv_available is None:
             try:
                 import cv2
-                _VisualLibAvailability._is_opencv_available = True
+                _VisualLibAvailability.is_opencv_available = True
             except ImportError:
-                _VisualLibAvailability._is_opencv_available = False
-        return _VisualLibAvailability._is_opencv_available
+                _VisualLibAvailability.is_opencv_available = False
+        return _VisualLibAvailability.is_opencv_available
 
     @staticmethod
     def pytorch_available() -> bool:
-        if _VisualLibAvailability._is_torchvision_available is None:
+        if _VisualLibAvailability.is_torchvision_available is None:
             try:
                 import torchvision
-                _VisualLibAvailability._is_torchvision_available = True
+                _VisualLibAvailability.is_torchvision_available = True
             except ImportError:
-                _VisualLibAvailability._is_torchvision_available = False
-        return _VisualLibAvailability._is_torchvision_available
+                _VisualLibAvailability.is_torchvision_available = False
+        return _VisualLibAvailability.is_torchvision_available
 
     @staticmethod
     def matplotlib_available() -> bool:
-        if _VisualLibAvailability._is_matplotlib_available is None:
+        if _VisualLibAvailability.is_matplotlib_available is None:
             try:
                 import matplotlib
-                _VisualLibAvailability._is_matplotlib_available = True
+                _VisualLibAvailability.is_matplotlib_available = True
             except ImportError:
-                _VisualLibAvailability._is_matplotlib_available = False
-        return _VisualLibAvailability._is_matplotlib_available
+                _VisualLibAvailability.is_matplotlib_available = False
+        return _VisualLibAvailability.is_matplotlib_available
 
     @staticmethod
     def pil_available() -> bool:
-        if _VisualLibAvailability._is_pil_available is None:
+        if _VisualLibAvailability.is_pil_available is None:
             try:
                 import PIL
-                _VisualLibAvailability._is_pil_available = True
+                _VisualLibAvailability.is_pil_available = True
             except ImportError:
-                _VisualLibAvailability._is_pil_available = False
-        return _VisualLibAvailability._is_pil_available
+                _VisualLibAvailability.is_pil_available = False
+        return _VisualLibAvailability.is_pil_available
 
     @staticmethod
     def ffmpeg_available() -> bool:
-        if _VisualLibAvailability._is_ffmpeg_available is None:
+        if _VisualLibAvailability.is_ffmpeg_available is None:
             from ..preprocess._ffmpeg_check import ffmpeg_available
-            _VisualLibAvailability._is_ffmpeg_available = ffmpeg_available
-        return _VisualLibAvailability._is_ffmpeg_available
+            _VisualLibAvailability.is_ffmpeg_available = ffmpeg_available
+        return _VisualLibAvailability.is_ffmpeg_available
 
     @staticmethod
     def skimage_available() -> bool:
-        if _VisualLibAvailability._is_skimage_available is None:
+        if _VisualLibAvailability.is_skimage_available is None:
             try:
                 import skimage
-                _VisualLibAvailability._is_skimage_available = True
+                _VisualLibAvailability.is_skimage_available = True
             except ImportError:
-                _VisualLibAvailability._is_skimage_available = False
-        return _VisualLibAvailability._is_skimage_available
+                _VisualLibAvailability.is_skimage_available = False
+        return _VisualLibAvailability.is_skimage_available
 
 
 class _VisualLibAvailability:
-    _is_opencv_available: Optional[bool] = None
-    _is_torchvision_available: Optional[bool] = None
-    _is_matplotlib_available: Optional[bool] = None
-    _is_pil_available: Optional[bool] = None
-    _is_ffmpeg_available: bool = None
-    _is_skimage_available: Optional[bool] = None
+    is_opencv_available: Optional[bool] = None
+    is_torchvision_available: Optional[bool] = None
+    is_matplotlib_available: Optional[bool] = None
+    is_pil_available: Optional[bool] = None
+    is_ffmpeg_available: bool = None
+    is_skimage_available: Optional[bool] = None
--- src/tensorneko_util/preprocess/__init__.py
+++ src/tensorneko_util/preprocess/__init__.py
@@ -1,8 +1,9 @@
 from ._ffmpeg_check import ffmpeg_available
 from .video import frames2video
 from .image import rgb2gray, rgb2gray_batch
+from .crop import crop_with_padding
 
-__all__ = ['ffmpeg_available', 'frames2video', 'rgb2gray', 'rgb2gray_batch']
+__all__ = ['ffmpeg_available', 'frames2video', 'rgb2gray', 'rgb2gray_batch', 'crop_with_padding']
 
 if ffmpeg_available:
     from .ffmpeg import ffmpeg_command, video2frames, merge_video_audio, resample_video_fps, mp32wav
