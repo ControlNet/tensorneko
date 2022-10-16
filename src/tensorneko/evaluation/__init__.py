454194c31f4a0f23c9b79f3d24087bd965946c96
ControlNet <smczx@hotmail.com>
2022-10-17 09:25:32 +1100

    impl FID metrics

--- src/tensorneko/evaluation/__init__.py
+++ src/tensorneko/evaluation/__init__.py
@@ -1,7 +1,7 @@
 from .iou import iou_1d, iou_2d
 from .psnr import psnr_video, psnr_image
 from .ssim import ssim_video, ssim_image
-from .enum import Reduction
+from .fid import FID
 
 __all__ = [
     "iou_1d",
@@ -10,5 +10,5 @@ __all__ = [
     "psnr_image",
     "ssim_video",
     "ssim_image",
-    "Reduction",
+    "FID",
 ]
