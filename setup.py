7a62353b8c3b2bf74185e499441bee68321619c5
ControlNet <smczx@hotmail.com>
2023-05-11 18:33:16 +1000

    fix versions in ci

--- setup.py
+++ setup.py
@@ -35,14 +35,14 @@ setuptools.setup(
             "version.txt"
         ]
     },
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
