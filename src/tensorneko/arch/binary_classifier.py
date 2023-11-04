1e3e50eee48b2e4fd2cf5a4bb73788efc4ea3755
ControlNet <smczx@hotmail.com>
2023-11-04 19:18:49 +1100

    add metrics for binary classifier

--- src/tensorneko/arch/binary_classifier.py
+++ src/tensorneko/arch/binary_classifier.py
@@ -4,6 +4,7 @@ from typing import Optional, Union, Sequence, Dict
 from torch import Tensor
 from torch.nn import BCEWithLogitsLoss
 from torch.optim import Adam
+from torchmetrics import Accuracy, F1Score, AUROC
 
 from ..neko_model import NekoModel
 
@@ -17,6 +18,9 @@ class BinaryClassifier(NekoModel, ABC):
         self.learning_rate = learning_rate
         self.distributed = distributed
         self.loss_fn = BCEWithLogitsLoss()
+        self.acc_fn = Accuracy(task="binary")
+        self.f1_fn = F1Score(task="binary")
+        self.auc_fn = AUROC(task="binary")
 
     @classmethod
     def from_module(cls, model, learning_rate: float = 1e-4, distributed=False):
@@ -27,9 +31,13 @@ class BinaryClassifier(NekoModel, ABC):
 
     def step(self, batch: Optional[Union[Tensor, Sequence[Tensor]]]) -> Dict[str, Tensor]:
         x, y = batch
-        y_hat = self(x)
-        loss = self.loss_fn(y_hat, y.unsqueeze(1))
-        return {"loss": loss}
+        y_hat = self(x).squeeze(1)
+        loss = self.loss_fn(y_hat, y)
+        prob = y_hat.sigmoid()
+        acc = self.acc_fn(prob, y)
+        f1 = self.f1_fn(prob, y)
+        auc = self.auc_fn(prob, y)
+        return {"loss": loss, "acc": acc, "f1": f1, "auc": auc}
 
     def training_step(self, batch: Optional[Union[Tensor, Sequence[Tensor]]] = None, batch_idx: Optional[int] = None,
         optimizer_idx: Optional[int] = None, hiddens: Optional[Tensor] = None
