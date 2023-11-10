4c7bc1c5c878633d0da47fce740d8f915af74eaa
ControlNet <smczx@hotmail.com>
2023-11-10 14:35:37 +1100

    fix crop_with_padding using numpy int for the input

--- src/tensorneko_util/preprocess/crop.pyi
+++ src/tensorneko_util/preprocess/crop.pyi
@@ -7,3 +7,9 @@ from numpy import ndarray
 def crop_with_padding(image: ndarray, x1: int, x2: int, y1: int, y2: int, pad_value: Union[int, float] = 0.,
     batch: bool = False
 ) -> ndarray: ...
+
+
+@overload
+def crop_with_padding(image: ndarray, x1: ndarray, x2: ndarray, y1: ndarray, y2: ndarray,
+    pad_value: Union[int, float] = 0., batch: bool = False
+) -> ndarray: ...
