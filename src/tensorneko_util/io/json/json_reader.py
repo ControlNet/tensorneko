0b1c2696954e4c9fd272babde4321e121ad0293b
ControlNet <smczx@hotmail.com>
2024-02-15 16:46:54 +1100

    impl jsonl reader

--- src/tensorneko_util/io/json/json_reader.py
+++ src/tensorneko_util/io/json/json_reader.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union, Optional
+from typing import Union, Optional, List
 
 from ...util.type import T
 
@@ -52,6 +52,67 @@ class JsonReader:
 
         return obj
 
+
+    @classmethod
+    def of_jsonl(cls, path: str, clazz: Optional[T] = None, encoding: str = "UTF-8") -> List[Union[T, dict, list]]:
+        """
+        Read jsonl files to ``list`` or ``dict``.
+
+        Args:
+            path (``str``): Jsonl file path.
+            clazz: (``T``, optional): The object of the jsonl read for. The type should be decorated by
+                :func:`json_data`. This should be ``T`` or ``List[T]`` or ``List[List[T]]``
+            encoding (``str``, optional): The encoding for python ``open`` function. Default: "UTF-8"
+
+        Returns:
+            ``List[dict]`` | ``List[list]`` | ``List[T]``: The object of given jsonl.
+        """
+        if clazz is not None:
+            if "typing.List[" in str(clazz):
+                with open(path, "r", encoding=encoding) as file:
+                    obj: list = [json.loads(line) for line in file]
+
+                inner_type = clazz.__args__[0]
+                if "typing.List[" in str(inner_type):
+                    inner_inner_type = inner_type.__args__[0]
+                    try:
+                        is_json_data = inner_type.is_json_data
+                    except AttributeError:
+                        is_json_data = False
+                    if is_json_data:
+                        obj = [[inner_inner_type(e) for e in each] for each in obj]
+
+                else:
+                    try:
+                        is_json_data = inner_type.is_json_data
+                    except AttributeError:
+                        is_json_data = False
+
+                    if is_json_data:
+                        obj = list(map(inner_type, obj))
+            else:
+                with open(path, "r", encoding=encoding) as file:
+                    obj = clazz([json.loads(line) for line in file])
+        else:
+            with open(path, "r", encoding=encoding) as file:
+                obj = [json.loads(line) for line in file]
+
+        return obj
+
     def __new__(cls, path: str, clazz: T = None, encoding: str = "UTF-8") -> Union[T, dict, list]:
-        """Alias of :meth:`~tensorneko_util.io.json.json_reader.JsonReader.of`."""
-        return cls.of(path, clazz, encoding)
+        """
+        Read json or jsonl file smartly.
+
+        Args:
+            path (``str``): Json or jsonl file path.
+            clazz: (``T``, optional): The object of the json read for. The type should be decorated by
+                :func:`json_data`. This should be ``T`` or ``List[T]`` or ``List[List[T]]``
+            encoding (``str``, optional): The encoding for python ``open`` function. Default: "UTF-8"
+
+        Returns:
+            ``dict`` | ``list`` | ``object``: The object of given json or jsonl.
+        """
+        if path.endswith(".jsonl"):
+            return cls.of_jsonl(path, clazz, encoding)
+        else:
+            return cls.of(path, clazz, encoding)
