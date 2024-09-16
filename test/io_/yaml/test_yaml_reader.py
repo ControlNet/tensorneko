2743db51fc96f22f4523d64b3a3439a68be3dc1e
ControlNet <smczx@hotmail.com>
2024-09-16 16:32:39 +1000

    update pathlib test

--- test/io_/yaml/test_yaml_reader.py
+++ test/io_/yaml/test_yaml_reader.py
@@ -1,5 +1,6 @@
 import os.path
 import unittest
+from pathlib import Path
 
 from tensorneko.io import read
 
@@ -8,4 +9,8 @@ class YamlReaderTest(unittest.TestCase):
 
     def test_read_yaml_as_dict(self):
         obj = read.yaml(os.path.join("test", "resource", "test_read_yaml", "test_read_yaml_as_dict.yaml"))
+        self.assertEqual(obj, {"x": 1, "y": 2, "z": "abc"})
+
+    def test_pathlib(self):
+        obj = read.yaml(Path("test") / "resource" / "test_read_yaml" / "test_read_yaml_as_dict.yaml")
         self.assertEqual(obj, {"x": 1, "y": 2, "z": "abc"})
